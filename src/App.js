0fd392bf813e883c7ac9a1946c4436bf11ae2ac1
Journey Cruz <journey.cruz@gmail.com>
2021-12-27 11:14:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./styles/App.css";
+import React from 'react';
 import CurrentTime from "./components/CurrentTime";
-import Header from "./components/Header";
+import Header from './components/Header';
+import "./styles/App.css";
 
 function App() {
   return (
